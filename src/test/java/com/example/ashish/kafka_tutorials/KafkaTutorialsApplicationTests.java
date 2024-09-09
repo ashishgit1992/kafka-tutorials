17829333e01471e143baf9ca7b14f90e4df61336
ashish rawat <ashish.rawat991@gmail.com>
2024-09-09 18:02:52 +0530

    adding dependencies and setting up the git

---
+++ src/test/java/com/example/ashish/kafka_tutorials/KafkaTutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ashish.kafka_tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaTutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
